bbb9683b785eaf26383b6dceca8036960c63a29e
Oliver Thomson Brown <o.brown@epcc.ed.ac.uK>
2025-03-14 18:41:12 +0000

    device_utils.c: corrected qindex_to_cstate to not rely on uninitialised values

--- src/device/device_utils.c
+++ src/device/device_utils.c
@@ -6,7 +6,7 @@
 
 int qindex_to_cstate(qindex state, cstate * creg, const size_t BIT_WIDTH) {
   size_t i = BIT_WIDTH-1;
-  while (state > 0 && i >= 0) {
+  while (i > 0) {
     creg[i] = state % 2;
     state /= 2;
     --i;
