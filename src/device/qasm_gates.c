1ec6683f9f5e1a143493be2c5e3760507bdc93c3
Oliver Thomson Brown <o.brown@epcc.ed.ac.uK>
2025-03-14 17:32:06 +0000

    Implemented QFT gates

--- src/device/qasm_gates.c
+++ src/device/qasm_gates.c
@@ -1,14 +1,25 @@
 #include "datatypes.h"
+#include "resources.h"
 #include "qasm_gates.h"
+#include "quest/include/operations.h"
+
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
 
 int hadamard(qubit * qh) {
+  applyHadamard(qregistry.registers[qh->registry_index], qh->offset);
   return 0;
 }
 
 int cphase(qubit * ctrl, qubit * target, const double THETA) {
+  Qureg qureg = qregistry.registers[ctrl->registry_index];
+  applyTwoQubitPhaseShift(qureg, ctrl->offset, target->offset, THETA);
   return 0;
 }
 
 int swap(qubit * a, qubit * b) {
+  Qureg qureg = qregistry.registers[a->registry_index];
+  applySwap(qureg, a->offset, b->offset);
   return 0;
 }
