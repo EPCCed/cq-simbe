9716668bdc20efa12c8bc4b6ab77a13a862b7552
Oliver Thomson Brown <o.brown@epcc.ed.ac.uK>
2025-03-14 19:00:50 +0000

    device_utils.c: fixed (hopefully the final) stupid bug in qindex_to_cstate

--- src/device/device_utils.c
+++ src/device/device_utils.c
@@ -2,14 +2,14 @@
 #include "datatypes.h"
 #include "quest/include/types.h"
 
+#ifndef NDEBUG
 #include <stdio.h>
+#endif
 
 int qindex_to_cstate(qindex state, cstate * creg, const size_t BIT_WIDTH) {
-  size_t i = BIT_WIDTH-1;
-  while (i > 0) {
+  for (int i = BIT_WIDTH-1; i >= 0; --i) {
     creg[i] = state % 2;
     state /= 2;
-    --i;
   }
 
   return state;
