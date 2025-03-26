21e44cb05b438605c7a4720ef305fb13b0c750ce
Oliver Thomson Brown <o.brown@epcc.ed.ac.uK>
2025-03-26 14:52:29 +0000

    test_host_env.c: added additional TEST_ASSERTs

--- tests/host/test_host_env.c
+++ tests/host/test_host_env.c
@@ -20,24 +20,34 @@ void test_initial_environment_flags(void) {
 
 void test_init(void) {
   TEST_ASSERT_EQUAL_INT(0, cq_init(1));
+  TEST_ASSERT_TRUE(cq_env.initialised);
+  TEST_ASSERT_FALSE(cq_env.finalised);
   return;
 }
 
 void test_double_init(void) {
   TEST_ASSERT_EQUAL_INT(1, cq_init(1));
+  TEST_ASSERT_TRUE(cq_env.initialised);
+  TEST_ASSERT_FALSE(cq_env.finalised);
   return;
 }
 
 void test_finalise(void) {
   TEST_ASSERT_EQUAL_INT(0, cq_finalise(0));
+  TEST_ASSERT_TRUE(cq_env.initialised);
+  TEST_ASSERT_TRUE(cq_env.finalised);
 }
 
 void test_double_finalise(void) {
   TEST_ASSERT_EQUAL_INT(1, cq_finalise(1));
+  TEST_ASSERT_TRUE(cq_env.initialised);
+  TEST_ASSERT_TRUE(cq_env.finalised);
   return;
 }
 
 void test_reinitialise(void) {
   TEST_ASSERT_EQUAL_INT(-1, cq_init(1));
+  TEST_ASSERT_TRUE(cq_env.initialised);
+  TEST_ASSERT_TRUE(cq_env.finalised);
   return;
 }
