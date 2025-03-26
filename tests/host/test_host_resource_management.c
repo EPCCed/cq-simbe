b7b5dd3fe828fb3c522bd7f04466ede94433b8b9
Oliver Thomson Brown <o.brown@epcc.ed.ac.uK>
2025-03-26 19:45:45 +0000

    test_host_resources: added tests of offset and N

--- tests/host/test_host_resource_management.c
+++ tests/host/test_host_resource_management.c
@@ -1,9 +1,8 @@
+#include <stdio.h>
 #include "unity.h"
 #include "datatypes.h"
 #include "host_ops.h"
 #include "test_host_resource_management.h"
-
-#include <stdio.h>
 
 qubit * g_qr = NULL;
 
@@ -25,34 +24,45 @@ void test_first_alloc_qureg(void) {
 }
 
 void test_first_free_qureg(void) {
-  TEST_ASSERT_EQUAL_INT(CQ_SUCCESS, free_qureg(g_qr));
+  TEST_ASSERT_EQUAL_INT(CQ_SUCCESS, free_qureg(&g_qr));
   TEST_ASSERT_NULL(g_qr);
   return;
 }
 
 void test_alloc_and_free_qureg(void) {
-  qubit * qr;
+  qubit * qr = NULL;
   size_t nqubits;
+  size_t idx;
+  char msg[12];
   
   nqubits = 1; 
   TEST_ASSERT_EQUAL_INT(CQ_SUCCESS, alloc_qureg(&qr, nqubits));
   TEST_ASSERT_NOT_NULL(qr);
-  TEST_ASSERT_EQUAL_size_t(nqubits, sizeof(qr)/sizeof(qubit));
-  TEST_ASSERT_EQUAL(CQ_SUCCESS, free_qureg(qr));
+  TEST_ASSERT_EQUAL_size_t(0, qr[0].offset);
+  TEST_ASSERT_EQUAL_size_t(nqubits, qr[0].N);
+  TEST_ASSERT_EQUAL(CQ_SUCCESS, free_qureg(&qr));
   TEST_ASSERT_NULL(qr); 
   
   nqubits = 2; 
   TEST_ASSERT_EQUAL_INT(CQ_SUCCESS, alloc_qureg(&qr, nqubits));
   TEST_ASSERT_NOT_NULL(qr);
-  TEST_ASSERT_EQUAL_size_t(nqubits, sizeof(qr)/sizeof(qubit));
-  TEST_ASSERT_EQUAL(CQ_SUCCESS, free_qureg(qr));
+  for (idx = 0; idx < nqubits; ++idx) {
+    sprintf(msg, "idx = %lu", idx);
+    TEST_ASSERT_EQUAL_size_t_MESSAGE(idx, qr[idx].offset, msg);
+    TEST_ASSERT_EQUAL_size_t_MESSAGE(nqubits, qr[idx].N, msg);
+  }
+  TEST_ASSERT_EQUAL(CQ_SUCCESS, free_qureg(&qr));
   TEST_ASSERT_NULL(qr);
  
   nqubits = 3;
   TEST_ASSERT_EQUAL_INT(CQ_SUCCESS, alloc_qureg(&qr, nqubits));
-  TEST_ASSERT_NOT_NULL(qr);
-  TEST_ASSERT_EQUAL_size_t(nqubits, sizeof(qr)/sizeof(qubit));
-  TEST_ASSERT_EQUAL(CQ_SUCCESS, free_qureg(qr));
+  TEST_ASSERT_NOT_NULL(qr);  
+  for (idx = 0; idx < nqubits; ++idx) {
+    sprintf(msg, "idx = %lu", idx);
+    TEST_ASSERT_EQUAL_size_t_MESSAGE(idx, qr[idx].offset, msg);
+    TEST_ASSERT_EQUAL_size_t_MESSAGE(nqubits, qr[idx].N, msg);
+  }
+  TEST_ASSERT_EQUAL(CQ_SUCCESS, free_qureg(&qr));
   TEST_ASSERT_NULL(qr);
  
   return;
@@ -64,7 +74,7 @@ void test_realloc_qureg(void) {
 
   alloc_qureg(&qr, NQ);
   TEST_ASSERT_EQUAL_INT(CQ_WARNING, alloc_qureg(&qr, NQ));
-  free_qureg(qr);
+  free_qureg(&qr);
 
   return;
 }
@@ -74,8 +84,8 @@ void test_double_free_qureg(void) {
   const size_t NQ = 3;
 
   alloc_qureg(&qr, NQ);
-  free_qureg(qr);
-  TEST_ASSERT_EQUAL_INT(CQ_WARNING, free_qureg(qr));
+  free_qureg(&qr);
+  TEST_ASSERT_EQUAL_INT(CQ_WARNING, free_qureg(&qr));
 
   return;
 }
