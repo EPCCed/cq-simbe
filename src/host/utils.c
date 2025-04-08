7bfbfa35d54501e6924fd871f6a2c497dc7a1cb4
Oliver Thomson Brown <o.brown@epcc.ed.ac.uk>
2025-04-08 14:43:20 +0100

    utils.c: removed debugging

--- src/host/utils.c
+++ src/host/utils.c
@@ -1,14 +1,9 @@
 #include "datatypes.h"
 #include "utils.h"
 
-#include <stdio.h>
-
 void init_creg(const size_t LENGTH, const cstate INIT_VAL, cstate * cr) {
-  printf("%s: LENGTH=%lu, INIT_VAL=%d\n", __func__, LENGTH, INIT_VAL);
   for (size_t i = 0; i < LENGTH; ++i) {
     cr[i] = INIT_VAL;
-    printf("\tcr[%lu] = %d\n", i, cr[i]);
   }
-  
   return;
 }
