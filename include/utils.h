51b4a121bf5a4b2237179ae34a4a4a5d092e0600
m-meller <mateusz.meller2@gmail.com>
2025-07-22 12:49:51 +0000

    Fixed typo in utils.h

--- include/utils.h
+++ include/utils.h
@@ -8,7 +8,7 @@ void init_creg(const size_t LENGTH, const cstate INIT_VAL, cstate * cr);
 void report_results(
     cstate const * const CR,
     const size_t NMEASURE,
-    const size_t NSHOTS)
+    const size_t NSHOTS);
 
 #define HANDLE_CQ_ERROR(x)                           \
         {                                           \
